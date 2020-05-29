4e4cf32264fd4c2627fa431ed9f9f91b2e38109d
rlransom <rlransom@ncsu.edu>
2020-05-29 11:51:32 -0400

    Committ

--- Data_Curation.R
+++ Data_Curation.R
@@ -8,16 +8,45 @@
 
 #Load necessary packages
 #install.packages("tidyverse")
-
+#Clear Envieronment
+remove(list = ls())
 
 
 #Load Raw Data from Clinton Research Station Site
 Clinton <- read.csv("RAW_Clinton.csv")
+
+#Inspect Data
+##Column names
+colnames(Clinton)
+##Structure of data
+str(Clinton)
+##Head of data
+head(Clinton)
+tail(Clinton)
+
+#Add New Column, "Location Name"
+Location_Name <-rep("clinton", nrow(Clinton))
+Clinton <- cbind(Location_Name, Clinton)
+
+
 #Load Raw Data from Kinston Research Station Site
 Kinston <- read.csv("RAW_Kinston.csv")
 
+#Inspect Data
+##Column names
+colnames(Kinston)
+##Structure of data
+str(Kinston)
+##Head of data
+head(Kinston)
+tail(Kinston)
+
+#Add New Column, "Location Name"
+Location_Name <-rep("kinston", nrow(Kinston))
+Clinton <- cbind(Location_Name, Kinston)
+
 #Join tables
-Monthly_Weather <- full_join(Clinton, Kinston)
+Monthly_Weather <- dplyr::full_join(Clinton, Kinston)
 
 #Filter out dates before June 2018
   
