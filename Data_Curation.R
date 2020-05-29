454f9a48c68fef429e350f387b6c60e02ab93536
rlransom <rlransom@ncsu.edu>
2020-05-29 10:49:51 -0400

    Commit

---
+++ Data_Curation.R
@@ -0,0 +1,6 @@
+###############################################################
+##Weather Data for 2018 SP Growing Season - Clinton and Kinston
+###############################################################
+
+#Load Raw Data
+Clinton <- read.csv("RAW_Clinton.csv")
