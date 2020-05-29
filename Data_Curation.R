6d9044fa213ab231887d43d64e86f8b445bf46a8
rlransom <rlransom@ncsu.edu>
2020-05-29 11:04:26 -0400

    Commit

--- Data_Curation.R
+++ Data_Curation.R
@@ -2,5 +2,23 @@
 ##Weather Data for 2018 SP Growing Season - Clinton and Kinston
 ###############################################################
 
-#Load Raw Data
+#Driving Question: What weather factors greately impact the shape and growth of sweet potatoes?
+
+###############################################################
+
+#Load necessary packages
+#install.packages("tidyverse")
+
+
+
+#Load Raw Data from Clinton Research Station Site
 Clinton <- read.csv("RAW_Clinton.csv")
+#Load Raw Data from Kinston Research Station Site
+Kinston <- read.csv("RAW_Kinston.csv")
+
+#Join tables
+Monthly_Weather <- full_join(Clinton, Kinston)
+
+#Filter out dates before June 2018
+  
+  
