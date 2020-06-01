81e65f8b0db7119f46e5cd3a6c13cc479bb2b2cd
rlransom <rlransom@ncsu.edu>
2020-06-01 12:10:45 -0400

    commit

--- Data_Curation.R
+++ Data_Curation.R
@@ -53,7 +53,25 @@ Monthly_Weather <- dplyr::full_join(Clinton, Kinston)
 
 #Rename columns
 colnames(Monthly_Weather)
-Monthly_Weather <- dplyr::rename(Monthly_Weather, 
+Monthly_Weather <- dplyr::rename(Monthly_Weather, "Date" = "Date.Time..EST.", 
+                                 "Avg daily air temperature 2m" = "monthly.AVG.of.2m.Average.Daily.Temperature..F.", 
+                                 "Daily max air temperature 2m" =  "monthly.AVG.of.2m.Daily.max.air.temperature..F.", 
+                                 "Daily min air temperature 2m" = "monthly.AVG.of.2m.Daily.Min.Temperature..F.", 
+                                 "Avg daily air temperature 10m" = "monthly.AVG.of.10m.Average.Daily.Temperature..F.", 
+                                 "Daily max air temperature 10m" =  "monthly.AVG.of.10m.Daily.max.air.temperature..F.", 
+                                 "Daily min air temperature 10m" = "monthly.AVG.of.10m.Daily.Min.Temperature..F.", 
+                                 "Heating degree days" = "X2m.Heating.Degree.Days..F.", 
+                                 "Cooling degree days" = "X2m.Cooling.Degree.Days..F.", 
+                                 "Relative humidity daily average" = "monthly.AVG.of.2m.Relative.Humidity.Daily.Avg..percent.", 
+                                 "Daily max relative humidity" = "monthly.AVG.of.2m.Daily.max.Relative.Humidity..percent.", 
+                                 "Daily min relative humidity" = "monthly.AVG.of.2m.Daily.min.Relative.Humidity..percent.", 
+                                 "Daily precipitation" = "monthly.SUM.of.1m.Daily.Precipitation..in.", 
+                                 "Soil temperature daily mean" = , 
+                                 "Soil temperature daily min" = , 
+                                 "Soil temperature daily max" =, 
+                                 "Soil moisture daily average" = , 
+                                 "Soil moisture daily min" = , 
+                                 "Soil moisture daily max" = )
 
 
 #Change Date Column from "Character" to "Date" class
