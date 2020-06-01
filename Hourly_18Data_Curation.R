6aed2497a6c5c448e892371f35512fe28851bea4
rlransom <rlransom@ncsu.edu>
2020-06-01 12:33:09 -0400

    Commit

---
+++ Hourly_18Data_Curation.R
@@ -0,0 +1,13 @@
+###############################################################
+##Hourly Weather Data for 2018 SP Growing Season - Clinton and Kinston
+###############################################################
+
+#Driving Question: What weather factors greatly impact the shape and growth of sweetpotatoes?
+
+###############################################################
+#Clear Environment
+remove(list = ls())
+
+#Load necessary packages
+install.packages("tidyverse")
+library(readr)
---
+++ Monthly_18Data_Curation.R
@@ -0,0 +1,43 @@
+###############################################################
+##Monthly Weather Data for 2018 SP Growing Season - Clinton and Kinston
+###############################################################
+
+#Driving Question: What weather factors greatly impact the shape and growth of sweetpotatoes?
+
+###############################################################
+#Clear Environment
+remove(list = ls())
+
+#Load necessary packages
+install.packages("tidyverse")
+library(readr)
+
+
+#Load Raw Data from Clinton Research Station Site
+Clinton <- read_csv("RAW_Clinton.csv")
+
+#Add location identifier
+Location_Name <-rep("clinton", nrow(Clinton))
+Clinton <- cbind(Location_Name, Clinton)
+
+#Load Raw Data from Kinston Research Station Site
+Kinston <- read_csv("RAW_Kinston.csv")
+
+#Add location identifier
+Location_Name <-rep("kinston", nrow(Kinston))
+Kinston <- cbind(Location_Name, Kinston)
+
+#Create combined data table
+Monthly_Weather <- dplyr::full_join(Clinton, Kinston)
+
+#Inspect Data
+##Column names
+colnames(Monthly_Weather)
+##Structure of data
+str(Monthly_Weather)
+##Head/Tain of data
+head(Monthly_Weather)
+tail(Monthly_Weather)
+
+
+#Change Date Column from "Character" to "Date" class
