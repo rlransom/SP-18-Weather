8b78cb84a145d27b0dd75a2722c7d2ae4f839b62
rlransom <rlransom@ncsu.edu>
2020-06-11 12:39:36 -0400

    1

--- Hourly_18Data_Curation.R
+++ Hourly_18Data_Curation.R
@@ -9,12 +9,16 @@
 remove(list = ls())
 
 #Load necessary packages
-install.packages("tidyverse")
+#install.packages("tidyverse")
+#install.packages("naniar")
+library("tidyverse")
 library("readr")
 library("readxl")
+library("naniar")
+library("lubridate")
 
 #Load Hourly Data from Clinton Research Station Site
-Clinton <- read_excel("Processed_Clinton18_Hourly.xlsx")
+Clinton <- read_excel("RFriendly_Clinton18_Hourly.xlsx")
 
 #Inspect Data
 ##Column names
@@ -27,5 +31,25 @@ tail(Clinton)
 ##Summarize
 summary(Clinton)
 
-#Replace character entries with NA
-replace_with_na_if()
+
+#Replace character entries ("missing") with NA
+Clinton <- naniar::replace_with_na_if(Clinton, .predicate = is.character, condition = ~.x %in% ("missing"))
+
+#Change column type to numeric
+names <- colnames(Clinton)[c(-1)]
+Clinton <- Clinton %>%
+  mutate_at(names, as.numeric)
+
+#Summarize
+summary(Clinton)
+#We now see some basic stats about each variable
+
+
+#Preliminary Visualization
+Clinton %>%
+  ggplot() +
+  geom_point(mapping = aes(x = DateTime, y = Air_Temp_Avg)) +
+  xlab("Date") +
+  ylab("Temperature (F)") +
+  ggtitle("Average Temperature")+
+  theme_light()
