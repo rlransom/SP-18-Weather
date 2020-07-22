bc919d0e732c50e510e0cff95bb45a09b25eaebf
rlransom <rlransom@ncsu.edu>
2020-07-22 16:12:47 -0400

    1

--- Weather_Data_Processing.R
+++ Weather_Data_Processing.R
@@ -70,6 +70,12 @@ Data <- Data %>%
 #Add week column
 Data <- Data %>%
   mutate(Week = lubridate::week(Date))
+#Keep a copy to use later
+Data_with_weeks <- Data %>%
+  mutate(Week = lubridate::week(Date))
+
+
+
 #Select columns to take sum
 s <- Data %>%
   select(Week, Precipitation)%>%
@@ -112,16 +118,74 @@ Data <- as.data.frame(Data)
 #Reformat Data
 #Clinton 2018 Planting date: June 14, 2018. The 24th week of 2018.
 #Remove weeks before this
-
 Data <- Data %>%
   filter(Week >= 24)
+Data_with_weeks <- Data_with_weeks %>%
+  filter(Week >= 24)
+
+
 #Clinton 2018 Harvest ended in week 42
 Data <- Data %>%
+  filter(Week <= 42)
+Data_with_weeks <- Data_with_weeks %>%
   filter(Week <= 42)
 
 #Week column should signifity weeks since planting week, so subtract 24 frome each
 Data$Week <- Data$Week - 24
+Data_with_weeks$Week <- Data_with_weeks$Week - 24
 
+#Remove Temp avg b/c it's same as temp DB
+data <- Data[-5,]
+
+#########################
+#Summary Statistsics
+summary(data)
+
+#We want prettier summary
+#Install and load skimr
+###install.packages("skimr")
+library(skimr)
+library(ggplot2)
+
+skim(data)
+
+#Temperature
+Temperature <- data$Air_Temp_DB
+#Summary Statistics
+summary(Temperature)
+#Make histogram
+ggplot(data, aes(x=Air_Temp_DB))+
+  geom_histogram(binwidth=5, fill="Grey", color="black")+
+  ggtitle("2018 Temperature Histogram")+
+  xlab("Air Temperature (F)")+
+  ylab("Frequency (Weeks)")+
+  theme_bw()
+#Make Boxplot
+ggplot(Data_with_weeks, aes(x=Week, y=Air_Temp_DB, group=Week))+
+  geom_boxplot()+
+  ggtitle("2018 Temperature Boxplot")+
+  xlab("Week Since Planting")+
+  ylab("Air Temperature (F)")+
+  theme_bw()
+
+#Soil Moisture
+Soil_Moisture <- data$Soil_Moisture_Avg
+#Summary Statistics
+summary(Soil_Moisture)
+#Make histogram
+ggplot(data, aes(x=Soil_Moisture_Avg))+
+  geom_histogram(binwidth=.05, fill="Grey", color="black")+
+  ggtitle("2018 Soil Moisture Histogram")+
+  xlab("Soil Moisture (m3/m3)")+
+  ylab("Frequency (Weeks)")+
+  theme_bw()
+#Make Boxplot
+ggplot(Data_with_weeks, aes(x=Week, y=Soil_Moisture_Avg, group=Week))+
+  geom_boxplot()+
+  ggtitle("2018 Weekly Mean Soil Moisture Boxplots")+
+  xlab("Week Since Planting")+
+  ylab("Soil Moisture (m3/m3)")+
+  theme_bw()
 
 ##Export tidied data
 write_csv(Data, path = "Exported_Data/Tidy_Clinton_2018.csv")
